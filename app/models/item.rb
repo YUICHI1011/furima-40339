b7574d61460a78b5bc6afc65e9fd097641f44500
YUICHI1011 <159126508+YUICHI1011@users.noreply.github.com>
2024-03-04 16:28:43 +0900

    ユーザー管理機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ApplicationRecord
   validates :category_id, presence: true
   validates :status_id, presence: true
   validates :price, presence: true
-  validates :user, presence: true
-  validates :shipping_free_id,presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
+  # validates :user, presence: true
+  # validates :shipping_free_id,presence: true
+  # validates :prefecture_id, presence: true
+  # validates :shipping_day_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "には英字と数字の両方を含めて設定してください" }
   validates :password, format: { without: /[^ -~｡-ﾟ]+/, message: "には半角文字のみを使用してください" }
   validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字（漢字・ひらがな・カタカナ）で入力してください" }
