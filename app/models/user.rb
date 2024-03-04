3e934842a76de6328ed5ef0d73e1b6844ca0719e
YUICHI1011 <159126508+YUICHI1011@users.noreply.github.com>
2024-03-04 17:04:59 +0900

    ユーザー管理機能の再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "には英字と数字の両方を含めて設定してください" }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ , message: "には英字と数字の両方を含めて設定してください" }
   validates :password, format: { without: /[^ -~｡-ﾟ]+/, message: "には半角文字のみを使用してください" }
-  validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字（漢字・ひらがな・カタカナ）で入力してください" }
-  validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字（漢字・ひらがな・カタカナ）で入力してください" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龯]+\z/ , message: "は全角文字（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龯]+\z/ , message: "は全角文字（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "は全角カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "は全角カタカナで入力してください" }
   validates :birthday, presence: true
 
   devise :database_authenticatable, :registerable,
