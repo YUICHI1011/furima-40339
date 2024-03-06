2f8c15dbc63f93f1912af10611f25cc7ca9676da
YUICHI1011 <159126508+YUICHI1011@users.noreply.github.com>
2024-03-06 16:32:19 +0900

    console.logの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,13 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OK");
 
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = Math.floor(priceInput.value * 0.1);
     const addTaxDom = document.getElementById("add-tax-price");
     const profit = Math.floor(priceInput.value - inputValue);
-    console.log(priceInput) 
     const profitDom = document.getElementById("profit");
     addTaxDom.innerHTML = inputValue;
     profitDom.innerHTML = profit;
-    console.log(inputValue);
   
   });
 });
