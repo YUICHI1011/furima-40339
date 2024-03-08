f800c0e6496056275c136cb65478865a94675a42
YUICHI1011 <159126508+YUICHI1011@users.noreply.github.com>
2024-03-08 14:12:41 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! ,except:[:index, :show]
+  before_action :correct_user, only: [:edit, :update]
   
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,10 +23,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :price, :shipping_fee_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 
+  def correct_user
+    @item = Item.find(params[:id])
+    redirect_to(root_url) unless current_user == @item.user
+  end
 end
