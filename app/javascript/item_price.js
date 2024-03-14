148f114276554dac16bbc6fb3429eedfcc261e32
YUICHI1011 <159126508+YUICHI1011@users.noreply.github.com>
2024-03-14 15:46:45 +0900

    JavaScriptの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,16 @@
-window.addEventListener('turbo:load', () => {
+const pay = () => {
 
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = Math.floor(priceInput.value * 0.1);
-    const addTaxDom = document.getElementById("add-tax-price");
-    const profit = Math.floor(priceInput.value - inputValue);
-    const profitDom = document.getElementById("profit");
-    addTaxDom.innerHTML = inputValue;
-    profitDom.innerHTML = profit;
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+const inputValue = Math.floor(priceInput.value * 0.1);
+const addTaxDom = document.getElementById("add-tax-price");
+const profit = Math.floor(priceInput.value - inputValue);
+const profitDom = document.getElementById("profit");
+addTaxDom.innerHTML = inputValue;
+profitDom.innerHTML = profit;
   
   });
-});
+};
+
+window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
